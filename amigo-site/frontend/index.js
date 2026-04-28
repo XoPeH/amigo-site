891da855b6f93118395d31a4a9a84968612e67e9
XoPeH <blmzxo@gmail.com>
2026-04-28 12:26:14 +0300

    Add files via upload

---
+++ amigo-site/frontend/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:"60px"}}>
+      <h1>AMIGO</h1>
+      <p>Global Industrial Supply System</p>
+      <a href="/products">Catalog</a>
+    </div>
+  );
+}
